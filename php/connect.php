90e105cc9341d25966819c26b56c34395cab68c4
Somayahak <73739100+Somayahak@users.noreply.github.com>
2022-12-27 15:03:44 +0300

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,20 @@
 <?php
 
+
+$Email = $_POST['Email'];
+$Password = $_POST['Password'];
+$ConfPassword = $_POST['ConfPassword'];
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$Gender = $_POST['Gender'];
+$Phone = $_POST['Phone'];
+
+$education = $_POST['education'];
+$skills = $_POST['skills'];
+
+///////////////////////////////////////////////////////////////
+
 session_start();
 $servername = "sql6.freesqldatabase.com";
 $username = "sql6586681";
@@ -8,8 +23,23 @@ $dbname = "sql6586681";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-    $query = "INSERT INTO users VALUES('1','$firstname','$lastname','$Email','$Password','$Phone','$Gender','$education',NULL,NULL,NULL,NULL,NULL)";
 
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
 
+else{
+$sql = "INSERT INTO users (userId, firstName, lastName, email, password, phone, gender, education, cardHolderName, cardNo, expireDate, userDescription, rating)
+VALUES ('1','$firstname','$lastname','$Email','$Password','$Phone','$Gender','$education',NULL,NULL,NULL,NULL,NULL)";
 
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+} 
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
 ?>
