781b8ce61389f42c6328abbf2ed59691d59aefb7
Somayahak <73739100+Somayahak@users.noreply.github.com>
2022-12-27 15:19:39 +0300

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,17 +1,17 @@
 <?php
 
 
-$Email = $_POST['Email'];
-$Password = $_POST['Password'];
-$ConfPassword = $_POST['ConfPassword'];
+// $Email = $_POST['Email'];
+// $Password = $_POST['Password'];
+// $ConfPassword = $_POST['ConfPassword'];
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$Gender = $_POST['Gender'];
-$Phone = $_POST['Phone'];
+// $firstname = $_POST['firstname'];
+// $lastname = $_POST['lastname'];
+// $Gender = $_POST['Gender'];
+// $Phone = $_POST['Phone'];
 
-$education = $_POST['education'];
-$skills = $_POST['skills'];
+// $education = $_POST['education'];
+// $skills = $_POST['skills'];
 
 ///////////////////////////////////////////////////////////////
 
@@ -24,14 +24,17 @@ $dbname = "sql6586681";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if (mysqli_connect_error()){
-die('Connect Error ('. mysqli_connect_errno() .') '
-. mysqli_connect_error());
-}
+// if (mysqli_connect_error()){
+// die('Connect Error ('. mysqli_connect_errno() .') '
+// . mysqli_connect_error());
+// }
 
-else{
-$sql = "INSERT INTO users (userId, firstName, lastName, email, password, phone, gender, education, cardHolderName, cardNo, expireDate, userDescription, rating)
-VALUES ('1','$firstname','$lastname','$Email','$Password','$Phone','$Gender','$education',NULL,NULL,NULL,NULL,NULL)";
+// else{
+// $sql = "INSERT INTO users (userId, firstName, lastName, email, password, phone, gender, education, cardHolderName, cardNo, expireDate, userDescription, rating)
+// VALUES ('1','$firstname','$lastname','$Email','$Password','$Phone','$Gender','$education',NULL,NULL,NULL,NULL,NULL)";
+
+$sql = INSERT INTO users (userId, firstName, lastName, email, password, phone, gender, education, cardHolderName, cardNo, expireDate, userDescription, rating) 
+  VALUES ('1', 'somayah', 'hak', 'somahakami@gmail.com', 'Ar@616382', '0555011252', 'female', 'bachelor', NULL, NULL, NULL, NULL, NULL);
 
 if ($conn->query($sql)){
 echo "New record is inserted sucessfully";
@@ -40,6 +43,6 @@ else{
 echo "Error: ". $sql ."
 ". $conn->error;
 }
-$conn->close();
-}
+// $conn->close();
+// }
 ?>
