cd1182530a09bda423208a1a072583427eed98db
Somayahak <73739100+Somayahak@users.noreply.github.com>
2022-12-27 15:30:07 +0300

    Update registeration.php

--- php/registeration.php
+++ php/registeration.php
@@ -1,55 +1,38 @@
 <?php
-
-require 'connect.php';
-// if(!empty($_SESSION["Email"])){
-//   header("Location: ..//HTML/login.html");
-// } 
-
-
-
-$Email = $_POST['Email'];
-$Password = $_POST['Password'];
-$ConfPassword = $_POST['ConfPassword'];
-
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$Gender = $_POST['Gender'];
-$Phone = $_POST['Phone'];
-
-$education = $_POST['education'];
-$skills = $_POST['skills'];
-
-
-$duplicate = mysqli_query($conn, "SELECT * FROM Users WHERE Email = '$Email'");
-if(mysqli_num_rows($duplicate) > 0){
-  echo
-  "<script> alert('Username or Email Has Already Taken'); </script>";
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+if (!empty($password)){
+$host = "sql6.freesqldatabase.com";
+$dbusername = "sql6586681";
+$dbpassword = "d71eJpbkAF";
+$dbname = "sql6586681";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
 }
 else{
-    $query = "INSERT INTO users VALUES('1','$firstname','$lastname','$Email','$Password','$Phone','$Gender','$education',NULL,NULL,NULL,NULL,NULL)";
-    mysqli_query($conn, $query);
-      echo "<script>
-      alert('Registration Successful');
-      window.location.href='..//HTML/profile.html';
-      </script>";
-  }
-
-
-// // Database connection
-// // $conn = new mysqli('localhost','root','','test');
-// if($conn->connect_error){
-//     echo "$conn->connect_error";
-//     die("Connection Failed : ". $conn->connect_error);
-// } else {
-//     // $stmt = $conn->prepare("insert into users(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-//     // $stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
-//     $query = "INSERT INTO users VALUES('1','$firstname','$lastname','$Email','$Password','$Phone','$Gender','$education',NULL,,NULL,NULL,NULL,NULL)";
-//     mysqli_query($conn, $query);
-//     $execval = $stmt->execute();
-//     echo $execval;
-//     echo "Registration successfully...";
-//     $stmt->close();
-//     $conn->close();
-// }
-
+$sql = "INSERT INTO signin (email, psw)
+values ('$username','$password')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
 ?>
