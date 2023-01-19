7639a97992cdaa5b421c089b9492e4d480e127ea
Somayahak <73739100+Somayahak@users.noreply.github.com>
2023-01-19 13:31:52 +0300

    Update prevProjects.php

--- HTML/prevProjects.php
+++ HTML/prevProjects.php
@@ -1,18 +1,29 @@
 <?php
 require '../php/connect.php';
-if(empty($_SESSION["Email"])){
-  header("Location: ..//HTML/logIn.html");
-} 
+
+if(!empty($_SESSION["Email"])){
+    $Email = $_SESSION["Email"];
+    $query = "SELECT * FROM users WHERE email = '{$Email}'";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result); 
+}
+else{
+  header("..//HTML/logIn.html");
+}
+
+// if(empty($_SESSION["Email"])){
+//   header("Location: ..//HTML/logIn.html");
+// } 
 	// insert a quote if submit button is clicked
 	if (isset($_POST['add'])) {
 		if (empty($_POST['prevProjectDescription']) || empty($_POST['projectName'])) {
 			$errors = "You must fill in the project name and description";
 		}else{
             $errors = "";
-            $idd=$row["userId"];
+            $uid=$row["userId"];
             $projectName = $_POST['projectName'];
 			$prevProjectDescription = $_POST['prevProjectDescription'];
-			$sql = "INSERT INTO userPrevProjects (id,prevProjectName,prevProjectDescription) VALUES ('29','$projectName','$prevProjectDescription')";
+			$sql = "INSERT INTO userPrevProjects (id,prevProjectName,prevProjectDescription) VALUES ('$uid','$projectName','$prevProjectDescription')";
 			mysqli_query($conn, $sql);
 			// header('location: ..//HTML/profile.php');
             
