1b59f31d79110e62caea568d9c2ad9972fde5ef2
Somayahak <73739100+Somayahak@users.noreply.github.com>
2023-01-19 15:41:15 +0300

    Update profile.php

--- HTML/profile.php
+++ HTML/profile.php
@@ -190,8 +190,9 @@ else{
                             <!--<a class="btn" href=""
                                 style="margin-left:80%; border-radius:20px; padding:2px 8px; background-color: #703589; font-size:10px; color: white;">View
                             </a>-->
+				<?php $i++; } ?>	
+
                         </div>
-		                <?php $i++; } ?>	
 
                     <button class="btn center" onclick="openForm()" style="border-radius:20px; padding:2px 8px; background-color: #703589; font-size:12px; color: white; width:65%; height:15%"> Add your previous project</button>
                         <div class="form-popup" id="myForm" style="padding-top:0; text-align:left; width:40%; height:70%; background-color:#EFF3F8;   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
