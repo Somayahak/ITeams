cd7d222d0b90137c432035d0d3eeec9d193d33b1
Somayahak <73739100+Somayahak@users.noreply.github.com>
2023-01-19 15:12:06 +0300

    Update profile.php

--- HTML/profile.php
+++ HTML/profile.php
@@ -3,6 +3,7 @@ require '../php/connect.php';
 
 if(!empty($_SESSION["Email"])){
     $Email = $_SESSION["Email"];
+    $userId = $_SESSION["userId"];
     $query = "SELECT * FROM users WHERE email = '{$Email}'";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_assoc($result); 
@@ -175,7 +176,7 @@ else{
                     <a href="..//HTML/myProjects.php" style="font-size: 13px; color:#703589; margin-left:35%;"> More</a>   
                         <?php 
                         // select all tasks if page is visited or refreshed
-                        $prvproject = mysqli_query($conn, "SELECT * FROM userPrevProjects WHERE projectUserId='30'");
+                        $prvproject = mysqli_query($conn, "SELECT * FROM userPrevProjects WHERE projectUserId=$userId");
 
                     $i = 1; while ($row = mysqli_fetch_array($prvproject)) { ?>
 
