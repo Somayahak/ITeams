aa6c24bd198c6fc8e63949c4966c0ea43eb0aef7
Somayahak <73739100+Somayahak@users.noreply.github.com>
2023-01-19 15:53:40 +0300

    Update profile.php

--- HTML/profile.php
+++ HTML/profile.php
@@ -174,7 +174,9 @@ else{
                                     Previous Projects
 
                     <a href="..//HTML/myProjects.php" style="font-size: 13px; color:#703589; margin-left:35%;"> More</a>
-			    <?php echo'hi';?>
+			    <div>
+				    <?php echo'hi';?>
+			    </div>
                         <?php 
                         // select all tasks if page is visited or refreshed
                         $prvproject = mysqli_query($conn, "SELECT * FROM userPrevProjects WHERE projectUserId='30'");
@@ -190,9 +192,8 @@ else{
                             <!--<a class="btn" href=""
                                 style="margin-left:80%; border-radius:20px; padding:2px 8px; background-color: #703589; font-size:10px; color: white;">View
                             </a>-->
+			    </div>
 				<?php $i++; } ?>	
-
-                        </div>
 
                     <button class="btn center" onclick="openForm()" style="border-radius:20px; padding:2px 8px; background-color: #703589; font-size:12px; color: white; width:65%; height:15%"> Add your previous project</button>
                         <div class="form-popup" id="myForm" style="padding-top:0; text-align:left; width:40%; height:70%; background-color:#EFF3F8;   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
