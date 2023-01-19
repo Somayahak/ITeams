31fabce656a1621934315039b5bf362931a5d9a7
Somayahak <73739100+Somayahak@users.noreply.github.com>
2023-01-19 14:17:47 +0300

    Update profile.php

--- HTML/profile.php
+++ HTML/profile.php
@@ -192,12 +192,12 @@ else{
                             </a>-->
                         </div>
 		                <?php $i++; } ?>	
+                    </div>
 
                     <button class="btn center" onclick="openForm()" style="border-radius:20px; padding:2px 8px; background-color: #703589; font-size:12px; color: white; width:65%; height:15%"> Add your previous project</button>
                         <div class="form-popup" id="myForm" style="padding-top:0; text-align:left; width:40%; height:70%; background-color:#EFF3F8;   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                     <button type="button" class="btn cancel" onclick="closeForm()" style="font-weight:bold; margin-left:93%; background-color: none; font-size:20px; color: grey; width:6%; height:6%">x</button>
                 <?php include("..//HTML/prevProjects.php");?>
-                    </div>
                 </div>
                 </div>
 
