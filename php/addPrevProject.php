f026753d79760e61d3439a29e519c718f2fbcfb8
Somayahak <73739100+Somayahak@users.noreply.github.com>
2023-01-19 23:37:50 +0300

    Update and rename addPrevProjevt.php to addPrevProject.php

---
+++ php/addPrevProject.php
@@ -0,0 +1,31 @@
+<?php
+require '../php/connect.php';
+
+if(!empty($_SESSION["Email"])){
+    $Email = $_SESSION["Email"];
+    $query = "SELECT * FROM users WHERE email = '{$Email}'";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result); 
+    $errors = "";
+}
+else{
+  header("..//HTML/logIn.html");
+}
+
+	// insert a quote if submit button is clicked
+if (isset($_POST['add'])) {
+	if (empty($_POST['prevProjectDescription']) || empty($_POST['projectName'])) {
+// 		$errors = "You must fill in the project name and description";
+    window.location.href='..//HTML/profile.php';
+
+	}else{
+$projectUserId=$row["userId"];
+$projectName = $_POST['projectName'];
+$prevProjectDescription = $_POST['prevProjectDescription'];
+$sql = "INSERT INTO userPrevProjects (projectUserId,prevProjectName,prevProjectDescription) VALUES ($projectUserId,'$projectName','$prevProjectDescription')";
+mysqli_query($conn, $sql);
+window.location.href='..//HTML/profile.php';
+            
+		}
+	}
+?>
