1d98fb7af88f0004cd5a2547c46b65e75fa7e9f2
Somayahak <73739100+Somayahak@users.noreply.github.com>
2023-01-20 15:53:09 +0300

    Update editProfile.php

--- HTML/editProfile.php
+++ HTML/editProfile.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>ITeams Find projects</title>
+    <title>ITeams Edit Profile</title>
     <!--person icon-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!-- Favicon-->
@@ -41,37 +41,18 @@
                 </div>
             </section>
         </header>
-
-        <!-- Search -->
-        <div class="containers" style="height:120px; margin-bottom:50px;  margin-top:30px">
-        <div class="containers" style="border-radius: 20px;height:100px; background-color: #fff;width:800px;">
-            <form action="..//HTML/.html">
-                <div class="container">
-                    <!--textbox-->
-                    <label for="search"><b style="color:#703589;">Browse</b>
-                        <br>
-                        <input type="text" id="search" name="search"
-                            style="width:600px;border: 1px solid none; color: black;background-color: #EFF3F8;border-radius: 10px; margin-right: 10px; height: 30px;font-size: 12px;" placeholder="Search For Projects"
-                            required="required">
-                        <button
-                            style="width:80px; border-radius: 20px; height:30px;background-color: #6CBBD2;font-size:12px;text-align:justify; padding:2px 20px;">Search</button>
-                        </div>              
-                </div>
-            </form>
-    </div>
          
         <!-- Recommneded Projects -->
         <form action="..//HTML/.html"> 
        <!--  <from action= "..//HTML/findProject.php" method="GET"> -->
             <div class="containers" style="height:670px; margin-bottom:50px;  margin-top:50px">
                 <div class="card flex" style="height:730px; background-color: #fff;width: 800px; border-radius: 20px;">
-                    <label for="freeform" style="font-size: 20px; color:#703589;"><b>Recommended Projects</b></label>
+                    <label for="freeform" style="font-size: 20px; color:#703589;"><b>Edit Profile</b></label>
                     <div class="px-3 ex1" style="height: inherit; margin-top: 5px;">
                         <div class="flex-container-v" style="padding: 10px; background-color:#fff;">
                            <!-- {% for j in movies_name %}-->
-                            <div class="wrapper"
-                                style=" background:#EFF3F8;width:fit-content;height: fit-content;margin-bottom: 20px;">
-                                <div class="box header" style="text-align: left;"><a class="nav-link" href=""><b>Web
+                            <div class="wrapper" style=" background:#EFF3F8;width:fit-content;height: fit-content;margin-bottom: 20px;">
+<!--                                 <div class="box header" style="text-align: left;"><a class="nav-link" href=""><b>Web
                                             Application
                                             That is Online Assignmnet Submission System</b></a></div>
 
@@ -93,95 +74,13 @@
                                     </span>
 
                                 </div>
-                                <div class="box content" style="text-align: left;  color: #313C41;padding-left: 10px;">
-                                    {{MEHAF}}
-                                </div>
-                                <div class="box footer" style="text-align: left;  color: #313C41;padding-left: 10px;">
-                                    <b>Budget 70.00
-                                        SAR -
-                                        570.00 SAR</b></div>
-
-                            </div>
-                            
-                           <!-- {% endfor %}-->
-                           
-                          <!--   <div class="wrapper"
-                                style=" background:#EFF3F8;width:fit-content;height: fit-content;margin-bottom: 20px;">
-                                <div class="box header" style="text-align: left;"><a class="nav-link" href=""><b>Web
-                                            Application
-                                            That is Online Assignmnet Submission System</b></a></div>
-                                <div class="box sidebar" style="text-align: left;  color: #313C41;">
-                                    <span style="margin:5px; padding:4px 10px;">
-                                        <span
-                                            style="margin-left:5px; border-radius: 20px; padding:2px 8px; font-weight:normal; border: 1.5px solid #6CBBD2;">
-                                            C </span>
-                                        <span
-                                            style="margin-left:5px; border-radius: 20px; padding:2px 8px; font-weight:normal; border: 1.5px solid #6CBBD2;">
-                                            Python </span>
-                                        <span
-                                            style="margin-left:5px; border-radius: 20px; padding:2px 8px; font-weight:normal; border: 1.5px solid #6CBBD2;">
-                                            HTML </span>
-                                        <span
-                                            style="margin-left:5px; border-radius: 20px; padding:2px 8px; font-weight:normal; border: 1.5px solid #6CBBD2;">
-                                            CSS </span>
-                                    </span>
-                                </div>
-                                <div class="box content" style="text-align: left;  color: #313C41;padding-left: 10px;">
-                                    In this
-                                    project
-                                    teachers will
-                                    assign assignment to students and students will upload the assignment online
-                                    with
-                                    the help of this web application
-                                </div>
-                                <div class="box footer" style="text-align: left;  color: #313C41;padding-left: 10px;">
-                                    <b>Budget 70.00
-                                        SAR -
-                                        570.00 SAR</b></div>
-                            </div>
-                            <div class="wrapper"
-                                style=" background:#EFF3F8;width:fit-content;height: fit-content;margin-bottom: 20px;">
-                                <div class="box header" style="text-align: left;"><a class="nav-link" href=""><b>Web
-                                            Application
-                                            That is Online Assignmnet Submission System</b></a></div>
-                                <div class="box sidebar" style="text-align: left;  color: #313C41;">
-                                    <span style="margin:5px; padding:4px 10px;">
-                                        <span
-                                            style="margin-left:5px; border-radius: 20px; padding:2px 8px; font-weight:normal; border: 1.5px solid #6CBBD2;">
-                                            C </span>
-                                        <span
-                                            style="margin-left:5px; border-radius: 20px; padding:2px 8px; font-weight:normal; border: 1.5px solid #6CBBD2;">
-                                            Python </span>
-                                        <span
-                                            style="margin-left:5px; border-radius: 20px; padding:2px 8px; font-weight:normal; border: 1.5px solid #6CBBD2;">
-                                            HTML </span>
-                                        <span
-                                            style="margin-left:5px; border-radius: 20px; padding:2px 8px; font-weight:normal; border: 1.5px solid #6CBBD2;">
-                                            CSS </span>
-                                    </span>
-                                </div>
-                                <div class="box content" style="text-align: left;  color: #313C41;padding-left: 10px;">
-                                    In this
-                                    project
-                                    teachers will
-                                    assign assignment to students and students will upload the assignment online
-                                    with
-                                    the help of this web application
-                                </div>
-                                <div class="box footer" style="text-align: left;  color: #313C41;padding-left: 10px;">
-                                    <b>Budget 70.00
-                                        SAR -
-                                        570.00 SAR</b></div>
                             </div> -->
-
                         </div>
                     </div>
                 </div>
             </div>
             </div>
         </form>
-    <!--   </form>-->
-        <!-- </div> -->
     </main>
 
     <!-- Footer-->
