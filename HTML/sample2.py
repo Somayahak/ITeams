23740dd762422447973a9da9fb3456cc63864f11
Somayahak <73739100+Somayahak@users.noreply.github.com>
2023-01-20 09:46:28 +0300

    Update sample2.py

--- HTML/sample2.py
+++ HTML/sample2.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
   
 # making list of pokemons
-MEHAF= 'MEHAFMEHAFMEHAFMEHAFMEHAFMEHAF'
+AMAL= 'AmalllAmalll'
 
 # defining home page
 @app.route('/footer')
@@ -13,7 +13,7 @@ def homepage():
   
 # returning index.html and list
 # and length of list to html page
-    return render_template("/footer.html", MEHAF=MEHAF)
+    return render_template("/footer.html", AMAL=AMAL)
   
 if __name__ == '__main__':
     app.run()
